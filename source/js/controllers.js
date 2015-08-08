bd940d9faf1595490a4a9bcbb7302f9ebfb8b8ab
Don McLamb <mclamb.donald@gmail.com>
2015-08-07 22:36:51 -0400

    made changes and fixed errors to render, spelling errors in index.html

---
+++ source/js/controllers.js
@@ -0,0 +1,28 @@
+angular.module('F1FeederApp.controllers', [])
+.controller('driversController', function($scope){
+  $scope.driversList = [{
+    Driver: {
+      givenName: 'Sebastian',
+      familyName: 'Vettel'
+    },
+    points: 332,
+    nationality: 'German',
+    Constructors: [
+      {name: 'Red Bull'}
+    ]
+  },
+
+  {
+    Driver: {
+      givenName: 'Fernando',
+      familyName: 'Alonso'
+    },
+    points: 207,
+    nationality: 'Spanish',
+    Constructors: [
+      {name: 'Ferrari'}
+    ]
+  }
+
+];
+});
