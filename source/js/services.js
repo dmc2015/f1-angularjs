71d939ce3015c034f9101804770bd1cac3c78495
Don McLamb <mclamb.donald@gmail.com>
2015-08-08 09:39:44 -0400

    added gulpfile for jshint and fixed bugs in controller to render api

---
+++ source/js/services.js
@@ -0,0 +1,17 @@
+angular.module('F1FeederApp.services', [])
+  .factory('ergastAPIservice', function($http){
+    var ergastAPI = {};
+
+    ergastAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP',
+        // url: 'http://ergast.com/api/fl/2013/driverStandings.json?callback=JSON_CALLBACK'
+      url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+
+    };
+
+
+  return ergastAPI;
+
+});
