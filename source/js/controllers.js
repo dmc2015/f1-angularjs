d82434b9270ef32d5af749036d1c9a96f5dc3552
Don McLamb <mclamb.donald@gmail.com>
2015-08-08 10:40:21 -0400

    added name filter

--- source/js/controllers.js
+++ source/js/controllers.js
@@ -1,10 +1,22 @@
 angular.module('F1FeederApp.controllers', [])
-  .controller('driversController', function($scope, ergastAPIservice){
-    $scope.nameFilter = null;
-    $scope.driversList = [];
+.controller('driversController', function($scope, ergastAPIservice){
+  $scope.nameFilter = null;
+  $scope.driversList = [];
 
-    ergastAPIservice.getDrivers().success(function (response){
-      $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-    });
+
+
+
+  $scope.searchFilter = function(driver){
+    var keyword = new RegExp($scope.nameFilter, 'i');
+    return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+  };
+
+
+
+
+
+  ergastAPIservice.getDrivers().success(function (response){
+    $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+  });
 
 });
