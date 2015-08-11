5c6195eb67ac76bc5911a95c234a849207a5e38a
Don McLamb <mclamb.donald@gmail.com>
2015-08-11 18:45:01 -0400

    added list to work on demonym plugin

--- source/js/flagjson.js
+++ source/js/flagjson.js
@@ -261,206 +261,215 @@ var countries = {
 "Zambia":"zm",
 "Zimbabwe":"zw"};
 
-
+//if (nationality[i] === )
 
 var nationality = {
 "Spanish": countries.Spain,
 "German": countries.Germany,
 };
 
+// var nationality = [
+// "Spanish ", countries.Spain,
+// "German || Nazi ", countries.Germany,
+// ];
 
-Country 	Demonym
-Afghanistan 	Afghan
-Albania 	Albanian
-Algeria 	Algerian
-Andorra 	Andorran
-Angola 	Angolan
-Antigua and Barbuda 	Antiguans, Barbudans
-Argentina 	Argentine or Argentinean
-Armenia 	Armenian
-Australia 	Australian or Ozzie or Aussie
-Austria 	Austrian
-Azerbaijan 	Azerbaijani
-The Bahamas 	Bahamian
-Bahrain 	Bahraini
-Bangladesh 	Bangladeshi
-Barbados 	Barbadian or Bajuns
-Belarus 	Belarusian
-Belgium 	Belgian
-Belize 	Belizean
-Benin 	Beninese
-Bhutan 	Bhutanese
-Bolivia 	Bolivian
-Bosnia and Herzegovina 	Bosnian, Herzegovinian
-Botswana 	Motswana (singular), Batswana (plural)
-Brazil 	Brazilian
-Brunei 	Bruneian
-Bulgaria 	Bulgarian
-Burkina Faso 	Burkinabe
-Burundi 	Burundian
-Cambodia 	Cambodian
-Cameroon 	Cameroonian
-Canada 	Canadian
-Cape Verde 	Cape Verdian or Cape Verdean
-Central African Republic 	Central African
-Chad 	Chadian
-Chile 	Chilean
-China 	Chinese
-Colombia 	Colombian
-Comoros 	Comoran
-Congo, Republic of the 	Congolese
-Congo, Democratic Republic of the 	Congolese
-Costa Rica 	Costa Rican
-Cote d&quot;Ivoire 	Ivorian
-Croatia 	Croat or Croatian
-Cuba 	Cuban
-Cyprus 	Cypriot
-Czech Republic 	Czech
-Denmark 	Dane or Danish
-Djibouti 	Djibouti
-Dominica 	Dominican
-Dominican Republic 	Dominican
-East Timor 	East Timorese
-Ecuador 	Ecuadorean
-Egypt 	Egyptian
-El Salvador 	Salvadoran
-Equatorial Guinea 	Equatorial Guinean or Equatoguinean
-Eritrea 	Eritrean
-Estonia 	Estonian
-Ethiopia 	Ethiopian
-Fiji 	Fijian
-Finland 	Finn or Finnish
-France 	French or Frenchman or Frenchwoman
-Gabon 	Gabonese
-The Gambia 	Gambian
-Georgia 	Georgian
-Germany 	German
-Ghana 	Ghanaian
-Greece 	Greek
-Grenada 	Grenadian or Grenadan
-Guatemala 	Guatemalan
-Guinea 	Guinean
-Guinea-Bissau 	Guinea-Bissauan
-Guyana 	Guyanese
-Haiti 	Haitian
-Honduras 	Honduran
-Hungary 	Hungarian
-Iceland 	Icelander
-India 	Indian
-Indonesia 	Indonesian
-Iran 	Iranian
-Iraq 	Iraqi
-Ireland 	Irishman or Irishwoman or Irish
-Israel 	Israeli
-Italy 	Italian
-Jamaica 	Jamaican
-Japan 	Japanese
-Jordan 	Jordanian
-Kazakhstan 	Kazakhstani
-Kenya 	Kenyan
-Kiribati 	I-Kiribati
-Korea, North 	North Korean
-Korea, South 	South Korean
-Kosovo 	Kosovar
-Kuwait 	Kuwaiti
-Kyrgyz Republic 	Kyrgyz or Kirghiz
-Laos 	Lao or Laotian
-Latvia 	Latvian
-Lebanon 	Lebanese
-Lesotho 	Mosotho (plural Basotho)
-Liberia 	Liberian
-Libya 	Libyan
-Liechtenstein 	Liechtensteiner
-Lithuania 	Lithuanian
-Luxembourg 	Luxembourger
-Macedonia 	Macedonian
-Madagascar 	Malagasy
-Malawi 	Malawian
-Malaysia 	Malaysian
-Maldives 	Maldivan
-Mali 	Malian
-Malta 	Maltese
-Marshall Islands 	Marshallese
-Mauritania 	Mauritanian
-Mauritius 	Mauritian
-Mexico 	Mexican
-Federated States of Micronesia 	Micronesian
-Moldova 	Moldovan
-Monaco 	Monegasque or Monacan
-Mongolia 	Mongolian
-Montenegro 	Montenegrin
-Morocco 	Moroccan
-Mozambique 	Mozambican
-Myanmar (Burma) 	Burmese or Myanmarese
-Namibia 	Namibian
-Nauru 	Nauruan
-Nepal 	Nepalese
-Netherlands 	Netherlander, Dutchman, Dutchwoman, Hollander or Dutch (collective)
-New Zealand 	New Zealander or Kiwi
-Nicaragua 	Nicaraguan
-Niger 	Nigerien
-Nigeria 	Nigerian
-Norway 	Norwegian
-Oman 	Omani
-Pakistan 	Pakistani
-Palau 	Palauan
-Panama 	Panamanian
-Papua New Guinea 	Papua New Guinean
-Paraguay 	Paraguayan
-Peru 	Peruvian
-Philippines 	Filipino
-Poland 	Pole or Polish
-Portugal 	Portuguese
-Qatar 	Qatari
-Romania 	Romanian
-Russia 	Russian
-Rwanda 	Rwandan
-Saint Kitts and Nevis 	Kittian and Nevisian
-Saint Lucia 	Saint Lucian
-Samoa 	Samoan
-San Marino 	Sammarinese or San Marinese
-Sao Tome and Principe 	Sao Tomean
-Saudi Arabia 	Saudi or Saudi Arabian
-Senegal 	Senegalese
-Serbia 	Serbian
-Seychelles 	Seychellois
-Sierra Leone 	Sierra Leonean
-Singapore 	Singaporean
-Slovakia 	Slovak or Slovakian
-Slovenia 	Slovene or Slovenian
-Solomon Islands 	Solomon Islander
-Somalia 	Somali
-South Africa 	South African
-Spain 	Spaniard or Spanish
-Sri Lanka 	Sri Lankan
-Sudan 	Sudanese
-Suriname 	Surinamer
-Swaziland 	Swazi
-Sweden 	Swede or Swedish
-Switzerland 	Swiss
-Syria 	Syrian
-Taiwan 	Taiwanese
-Tajikistan 	Tajik or Tadzhik
-Tanzania 	Tanzanian
-Thailand 	Thai
-Togo 	Togolese
-Tonga 	Tongan
-Trinidad and Tobago 	Trinidadian or Tobagonian
-Tunisia 	Tunisian
-Turkey 	Turk or Turkish
-Turkmenistan 	Turkmen(s)
-Tuvalu 	Tuvaluan
-Uganda 	Ugandan
-Ukraine 	Ukrainian
-United Arab Emirates 	Emirian
-United Kingdom 	Briton
-United States 	American
-Uruguay 	Uruguayan
-Uzbekistan 	Uzbek or Uzbekistani
-Vanuatu 	Ni-Vanuatu
-Vatican City (Holy See) 	none
-Venezuela 	Venezuelan
-Vietnam 	Vietnamese
-Yemen 	Yemeni or Yemenite
-Zambia 	Zambian
-Zimbabwe 	Zimbabwean
+// function cTD(countryToDemonym, ){}
+
+// function dTC(demonymTocountry, ){}
+
+//
+// Country 	Demonym
+// "Afghanistan":	"Afghan"
+// "Albania":	"Albanian"
+// "Algeria":	"Algerian"
+// "Andorra":	"Andorran"
+// "Angola": 	"Angolan"
+// "Antigua"and Barbuda 	Antiguans, Barbudans"
+// "Argentina":	"Argentine or Argentinean"
+// "Armenia":	"Armenian"
+// "Australia":	"Australian or Ozzie or Aussie"
+// "Austria": 	"Austrian"
+// "Azerbaijan 	":"Azerbaijani"
+// "The "Bahamas :	"Bahamian"
+// "Bahrain": 	"Bahraini"
+// "Bangladesh":	"Bangladeshi"
+// "Barbados": 	"Barbadian or Bajuns"
+// "Belarus": 	"Belarusian"
+// "Belgium":	"Belgian"
+// "Belize":	"Belizean"
+// "Benin":	"Beninese"
+// "Bhutan":	"Bhutanese"
+// "Bolivia":	"Bolivian"
+// "Bosnia"and Herzegovina 	Bosnian, Herzegovinian"
+// "Botswana 	"Motswana (singular), Batswana (plural)"
+// "Brazil":	"Brazilian"
+// "Brunei":	"Bruneian"
+// "Bulgaria":	"Bulgarian"
+// "Burkina"Faso :	"Burkinabe"
+// "Burundi 	":"Burundian"
+// "Cambodia":	"Cambodian"
+// "Cameroon":	"Cameroonian"
+// "Canada":	"Canadian"
+// "Cape "Verde 	Cape Verdian or Cape Verdean"
+// "Central"African Republic :	"Central African"
+// "Chad":	"Chadian"
+// "Chile":	"Chilean"
+// "China":	"Chinese"
+// "Colombia":	"Colombian"
+// "Comoros":	"Comoran"
+// "Congo", Republic of the :	"Congolese"
+// "Congo", Democratic Republic of the :	"Congolese"
+// "Costa "Rica :	"Costa Rican"
+// "Cote"d&quot;Ivoire :	"Ivorian"
+// "Croatia 	"Croat or :"Croatian"
+// "Cuba":	"Cuban"
+// "Cyprus 	":"Cypriot"
+// "Czech"Republic :	"Czech"
+// "Denmark 	"Dane or :"Danish"
+// "Djibouti 	":"Djibouti"
+// "Dominica":	"Dominican"
+// "Dominican "Republic :	"Dominican"
+// "East "Timor :	"East Timorese"
+// "Ecuador":	"Ecuadorean"
+// "Egypt":	"Egyptian"
+// "El "Salvador :	"Salvadoran"
+// "Equatorial "Guinea :	"Equatorial Guinean or Equatoguinean"
+// "Eritrea":	"Eritrean"
+// "Estonia":	"Estonian"
+// "Ethiopia":	"Ethiopian"
+// "Fiji":	"Fijian"
+// "Finland":	"Finn or Finnish"
+// "France 	"French or :"Frenchman or Frenchwoman"
+// "Gabon":	"Gabonese"
+// "The "Gambia :	"Gambian"
+// "Georgia":	"Georgian"
+// "Germany":	"German"
+// "Ghana":	"Ghanaian"
+// "Greece 	":"Greek"
+// "Grenada":	"Grenadian or Grenadan"
+// "Guatemala":	"Guatemalan"
+// "Guinea":	"Guinean"
+// "Guinea"-Bissau :	"Guinea-Bissauan"
+// "Guyana":	"Guyanese"
+// "Haiti":	"Haitian"
+// "Honduras":	"Honduran"
+// "Hungary":	"Hungarian"
+// "Iceland":	"Icelander"
+// "India":	"Indian"
+// "Indonesia":	"Indonesian"
+// "Iran":	"Iranian"
+// "Iraq":	"Iraqi"
+// "Ireland":	"Irishman or Irishwoman or Irish"
+// "Israel":	"Israeli"
+// "Italy":	"Italian"
+// "Jamaica":	"Jamaican"
+// "Japan":	"Japanese"
+// "Jordan":	"Jordanian"
+// "Kazakhstan":	"Kazakhstani"
+// "Kenya":	"Kenyan"
+// "Kiribati":	"I-Kiribati"
+// "Korea", North :	"North Korean"
+// "Korea", South :	"South Korean"
+// "Kosovo":	"Kosovar"
+// "Kuwait":	"Kuwaiti"
+// "Kyrgyz"Republic :	"Kyrgyz or Kirghiz"
+// "Laos 	"Lao or: "Laotian"
+// "Latvia":	"Latvian"
+// "Lebanon":	"Lebanese"
+// "Lesotho":	"Mosotho (plural Basotho)"
+// "Liberia":	"Liberian"
+// "Libya":	"Libyan"
+// "Liechtenstein":	"Liechtensteiner"
+// "Lithuania":	"Lithuanian"
+// "Luxembourg":	"Luxembourger"
+// "Macedonia":	"Macedonian"
+// "Madagascar":	"Malagasy"
+// "Malawi":	"Malawian"
+// "Malaysia":	"Malaysian"
+// "Maldives":	"Maldivan"
+// "Mali":	"Malian"
+// "Malta":	"Maltese"
+// "Marshall "Islands :	"Marshallese"
+// "Mauritania":	"Mauritanian"
+// "Mauritius":	"Mauritian"
+// "Mexico":	"Mexican"
+// "Federated "States of Micronesia :	"Micronesian"
+// "Moldova":	"Moldovan"
+// "Monaco":	"Monegasque or Monacan"
+// "Mongolia":	"Mongolian"
+// "Montenegro":	"Montenegrin"
+// "Morocco":	"Moroccan"
+// "Mozambique":	"Mozambican"
+// "Myanmar "(Burma) :	"Burmese or Myanmarese"
+// "Namibia":	"Namibian"
+// "Nauru":	"Nauruan"
+// "Nepal":	"Nepalese"
+// "Netherlands":	"Netherlander, Dutchman, Dutchwoman, Hollander or Dutch (collective)"
+// "New "Zealand :	"New Zealander or Kiwi"
+// "Nicaragua":	"Nicaraguan"
+// "Niger":	"Nigerien"
+// "Nigeria":	"Nigerian"
+// "Norway":	"Norwegian"
+// "Oman":	"Omani"
+// "Pakistan":	"Pakistani"
+// "Palau":	"Palauan"
+// "Panama":	"Panamanian"
+// "Papua "New Guinea :	"Papua New Guinean"
+// "Paraguay":	"Paraguayan"
+// "Peru":	"Peruvian"
+// "Philippines":	"Filipino"
+// "Poland":	"Pole or Polish"
+// "Portugal":	"Portuguese"
+// "Qatar":	"Qatari"
+// "Romania":	"Romanian"
+// "Russia":	"Russian"
+// "Rwanda":	"Rwandan"
+// "Saint "Kitts and Nevis :	"Kittian and Nevisian"
+// "Saint "Lucia :	"Saint Lucian"
+// "Samoa":	"Samoan"
+// "San "Marino :	"Sammarinese or San Marinese"
+// "Sao "Tome and Principe :	"Sao Tomean"
+// "Saudi "Arabia :	"Saudi or Saudi Arabian"
+// "Senegal":	"Senegalese"
+// "Serbia":	"Serbian"
+// "Seychelles":	"Seychellois"
+// "Sierra "Leone 	:"Sierra Leonean"
+// "Singapore":	"Singaporean"
+// "Slovakia":	"Slovak or Slovakian"
+// "Slovenia":	"Slovene or Slovenian"
+// "Solomon "Islands :	"Solomon Islander"
+// "Somalia":	"Somali"
+// "South "Africa :	"South African"
+// "Spain":	"Spaniard or Spanish"
+// "Sri "Lanka :	"Sri Lankan"
+// "Sudan":	"Sudanese"
+// "Suriname":	"Surinamer"
+// "Swaziland":	"Swazi"
+// "Sweden":	"Swede or Swedish"
+// "Switzerland":	"Swiss"
+// "Syria":	"Syrian"
+// "Taiwan":	"Taiwanese"
+// "Tajikistan":	"Tajik or Tadzhik"
+// "Tanzania":	"Tanzanian"
+// "Thailand":	"Thai"
+// "Togo":	"Togolese"
+// "Tonga":	"Tongan"
+// "Trinidad"and Tobago :	"Trinidadian or Tobagonian"
+// "Tunisia":	"Tunisian"
+// "Turkey":	"Turk or Turkish"
+// "Turkmenistan":	"Turkmen(s)"
+// "Tuvalu":	"Tuvaluan"
+// "Uganda":	"Ugandan"
+// "Ukraine":	"Ukrainian"
+// "United"Arab Emirates :	"Emirian"
+// "United "Kingdom :	"Briton"
+// "United"States :	"American"
+// "Uruguay":	"Uruguayan"
+// "Uzbekistan":	"Uzbek or Uzbekistani"
+// Vanuatu :	"Ni-Vanuatu
+// "Vatican "City (Holy See) :	"none"
+// Venezuela :	"Venezuelan
+// "Vietnam":	"Vietnamese"
+// "Yemen":	"Yemeni or Yemenite"
+// "Zambia":	"Zambian"
+// "Zimbabwe":	"Zimbabwean"
--- thingstolookup.md
+++ thingstolookup.md
@@ -5,3 +5,23 @@ Mocking
 Making Directives
 Promises
 does angular have any special directives or built in features to deal with api's?
+
+
+flash cards
+
+directives
+application module
+view
+controller
+model
+mvc vs mvvc
+name spacing system, why we don't use scope
+asynchrounous
+restapi
+http requests
+single bind
+double bind
+
+notes
+
+model lives in side the controller and the controller lives in side the module
